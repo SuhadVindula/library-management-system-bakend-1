df3d0c56a78c10b85df815a386a588918fe35c1b
Suhad Rajapaksha <suhadrajapaksha@gmail.com>
2023-07-05 12:52:08 +0530

    setting-up

---
+++ book-service/book-service/src/test/java/lk/ijse/dep10/library/bookservice/BookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.dep10.library.bookservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
