249600d446a6c40f699a9a609e784e301d0b1b2b
Suhad Rajapaksha <suhadrajapaksha@gmail.com>
2023-07-06 11:27:04 +0530

    7th-commit

--- member-service/src/api/member-rest-controller.ts
+++ member-service/src/api/member-rest-controller.ts
@@ -1,43 +1,149 @@
-import express, {json, Router} from "express";
+import express, {json} from 'express';
 import cors from 'cors';
-import {Db, MongoClient} from "mongodb";
-import env from "dotenv";
-
+import {Collection, Document, MongoClient} from "mongodb";
+import env from 'dotenv';
+import {Member} from "../dto/member";
 
 env.config();
 
-const mongo:MongoClient=new MongoClient(process.env.APP_DB_URL!);
+let memberRepo: Collection<Member>;
 
-
-
-async function main() {
-    // Use connect method to connect to the server
+async function main(){
+    const mongo = new MongoClient(process.env.APP_DB_URL!)
     await mongo.connect();
-    console.log('Connected successfully to server');
-    const db:Db = mongo.db(process.env.APP_DB_NAME);
-
+    console.log("Successfully connected to the Mongo DB Server!");
+    memberRepo = mongo.db(process.env.APP_DB_NAME).collection('member');
 }
+
 main();
 
+export const router = express.Router();
 
-export const router: Router = express.Router();
-
-router.use(json());
 router.use(cors());
+router.use(json());
 
-router.get('/', (req, res) =>
-    res.send("<h1>Get Members</h>"));
+function validateMember(member: Member){
+    const validationErrors: Array<{field: string, error: string}> = [];
 
-router.post('/', (req, res) =>
-    res.send("<h1>Update Members</h>")
-);
+    if (!member._id?.trim())
+        validationErrors.push({ field: '_id',  error: "Member id can't be empty" });
+    if (!/^\d{9}[Vv]$/.test(member._id))
+        validationErrors.push({ field: '_id',  error: "Member id should be a valid nic" });
 
-router.patch('/:memberId', (req, res) =>
-    res.send("<h1>Update Members</h>")
-);
+    if (!member.name?.trim())
+        validationErrors.push({ field: 'name',  error: "Member name can't be empty" });
+    if (!/^[A-Za-z ]+$/.test(member.name))
+        validationErrors.push( { field: 'name',  error: "Invalid member name" });
 
-router.delete('/:memberId', (req, res) =>
-    res.send("<h1>Delete Members</h>")
-);
+    if (!member.address?.trim())
+        validationErrors.push( { field: 'address',  error: "Member address can't be empty" });
 
-router.get('/', (req, res) => res.send(alert("save")));
+    if (!member.contact?.trim())
+        validationErrors.push({ field: 'contact',  error: "Member contact can't be empty" });
+    if (!/^\d{3}-\d{7}$/.test(member.contact))
+        validationErrors.push( { field: 'contact',  error: "Invalid member contact number" });
+
+    return validationErrors;
+}
+
+router.post('/', async (req, res, next) => {
+    try{
+        const member = req.body as Member;
+        const validationErrorList = validateMember(member);
+        if (validationErrorList.length) throw {name: 'validation', errors: validationErrorList};
+
+        if (await memberRepo.findOne({_id: member._id})){
+            throw {name: 'conflict', message: `NIC=${member._id} already exists`};
+        }
+        if (await memberRepo.findOne({contact: member.contact})){
+            throw {name: 'conflict', message: `Contact=${member.contact} already exists`};
+        }
+
+        await memberRepo.insertOne(member);
+        res.sendStatus(201);
+    }catch (e:any){
+        if (e.name === 'validation') {
+            res.status(400).json(e.errors);
+        }else if (e.name === 'conflict'){
+            res.status(409).json(e.message);
+        }else{
+            next(e);
+        }
+    }
+});
+
+router.patch('/:memberId', async (req, res, next) => {
+    try{
+        const member = req.body as Member;
+        member._id = req.params.memberId;
+        const validationErrorList = validateMember(member);
+        if (validationErrorList.length) throw {name: 'validation', errors: validationErrorList};
+
+        if (!await memberRepo.findOne({_id: member._id}))
+            throw {name: 'notfound', message: `NIC=${member._id} doesn't not exist`};
+
+        if ((await memberRepo.findOne({contact: member.contact}))?._id !== member._id){
+            throw {name: 'conflict', message: `contact=${member.contact} already associated with another member`};
+        }
+
+        await memberRepo.updateOne({_id: member._id}, {$set: member});
+        res.sendStatus(204);
+    }catch (e:any){
+        if (e.name === 'validation') {
+            res.status(400).json(e.errors);
+        }else if (e.name === 'conflict') {
+            res.status(409).json(e.message);
+        }else if (e.name === 'notfound'){
+            res.status(404).json(e.message);
+        }else{
+            next(e);
+        }
+    }
+});
+
+router.delete('/:memberId', async(req, res, next) => {
+    try{
+        if (!await memberRepo.findOne({_id: req.params.memberId}))
+            throw {name: 'notfound', message: `NIC=${req.params.memberId} doesn't not exist`};
+
+        await memberRepo.deleteOne({_id: req.params.memberId});
+        res.sendStatus(204);
+    }catch (e:any){
+        if (e.name === 'validation') {
+            res.status(400).json(e.errors);
+        }else if (e.name === 'conflict') {
+            res.status(409).json(e.message);
+        }else if (e.name === 'notfound'){
+            res.status(404).json(e.message);
+        }else{
+            next(e);
+        }
+    }
+});
+
+router.get('/:memberId', async (req, res,next) => {
+    try{
+        if (!await memberRepo.findOne({_id: req.params.memberId}))
+            throw {name: 'notfound', message: `NIC=${req.params.memberId} doesn't not exist`};
+
+        res.json(await memberRepo.findOne({_id: req.params.memberId}));
+    }catch (e:any){
+        if (e.name === 'validation') {
+            res.status(400).json(e.errors);
+        }else if (e.name === 'conflict') {
+            res.status(409).json(e.message);
+        }else if (e.name === 'notfound'){
+            res.status(404).json(e.message);
+        }else{
+            next(e);
+        }
+    }
+});
+
+router.get('/', async (req, res) => {
+    let query = req.query.q ?? '';
+    res.json(await memberRepo.find({
+        $or:[{_id: {$regex: `${query}`}}, {name: {$regex: `${query}`}},
+            {address: {$regex: `${query}`}}, {contact: {$regex: `${query}`}}]
+    }).toArray());
+});
